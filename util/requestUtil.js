3cf4420de7ed531e980b18020fe81c3bb741e036
joao.teixeira <joao.teixeira@checkok.com.br>
2021-05-25 17:23:44 -0300

    Add cep utils

--- util/requestUtil.js
+++ util/requestUtil.js
@@ -1,15 +1,27 @@
-export default function checkUserParams(body){
+export function checkUserParams(body){
     if(body.name == null || body.name == "")
         throw new Error('Fill the name field')
     
     if(body.email == null || body.email == "")
         throw new Error('Fill the e-mail field')
     
-    validEmail(body.email)
+    emailValidator(body.email)
     return true
 }
 
-function validEmail(email){
+export function checkCepParams(cep){
+    if(cep == null || cep == "")
+        throw new Error("Fill the cep field")
+
+    return cepCleaner(cep)
+}
+
+function cepCleaner(cep){
+    var cleanedCep = cep.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
+    return cleanedCep
+}
+
+function emailValidator(email){
     var pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
     if(email.match(pattern))
         return true
