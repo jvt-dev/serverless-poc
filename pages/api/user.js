39c95f399c039a4cd783d728259d3daaf2fdd5a2
joao.teixeira <joao.teixeira@checkok.com.br>
2021-05-25 17:25:04 -0300

    Fix user endpoint

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,17 +1,17 @@
-import checkUserParams from '../../util/requestUtil'
+import { checkUserParams } from '../../util/requestUtil'
 
 export default function handler(req, res) {
   try{  
     const { method, body } = req
 
     switch (method){
-      case 'GET':
-        checkUserParams(body)
+      case 'POST':
+        checkUserParams(body) 
         res.status(200).json({name: body.name, email: body.email})
         break
       default:
-        res.setHeader('Allow', ['GET']);
-        res.status(405).json({message: `Método ${method} não permitido`})
+        res.setHeader('Allow', ['POST']);
+        res.status(405).json({message: `Method ${method} not allowed`})
     }
   } catch (error){
     res.status(422).json({ message: error.message })
